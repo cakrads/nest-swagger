145b4a3d9bb2430f78809ad6a2bcef490187eb02
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-01-26 23:10:18 +0700

    delete unused file

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-import { resolve } from 'path';
-import { writeFileSync, createWriteStream } from 'fs';
-import { get } from 'http';
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -21,46 +16,5 @@ async function bootstrap() {
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
-
-  // get the swagger json file (if app is running in development mode)
-  console.log(process.env.NODE_ENV);
-  if (process.env.NODE_ENV === 'development') {
-    const serverURL = "http://localhost:3000";
-
-    // write swagger ui files
-    // get(
-    //   `${serverURL}/api/swagger-ui-bundle.js`, function
-    //   (response) {
-    //   response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
-    //   console.log(
-    //     `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
-    //   );
-    // });
-
-    // get(`${serverURL}/api/swagger-ui-init.js`, function (response) {
-    //   response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
-    //   console.log(
-    //     `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
-    //   );
-    // });
-
-    // get(
-    //   `${serverURL}/api/swagger-ui-standalone-preset.js`,
-    //   function (response) {
-    //     response.pipe(
-    //       createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
-    //     );
-    //     console.log(
-    //       `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
-    //     );
-    //   });
-
-    // get(`${serverURL}/api/swagger-ui.css`, function (response) {
-    //   response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
-    //   console.log(
-    //     `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
-    //   );
-    // });
-  }
 }
 bootstrap();
